164e534e855cb0ebbb4d0b3e61d0cf32f36cb4de
Smit <qyburn17@gmail.com>
2019-05-29 16:05:44 +0530

    till 06

---
+++ Gulp/Tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require("gulp"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+cssvars = require("postcss-simple-vars"),
+nested = require("postcss-nested"),
+cssImport = require("postcss-import");
+
+gulp.task('styles', function(){
+	console.log('Changes in CSS or PostCSS.');
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([autoprefixer, cssImport, nested, cssvars]))
+		.on('error', function(errorInfo){
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
+	 	.pipe(gulp.dest('./app/temp/styles'));
+});
