7d17297940a98d4b597fe81679364edf89da3088
Smit <wsm.i.t172@gmail.com>
2019-06-02 12:42:46 +0530

    completed the header section both for large and moblies.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,4 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person' ; 
+import MobileMenu from './modules/MobileMenu';
 
-alert('This is a test for webpack automation.');
 
-class Adult extends Person {
-	payTaxes(){
-		console.log(this.name + ' now owes 0$ in taxes.');
-	}
-}
-
-var john = new Person("John Doe", "Green");
-john.greet();
-
-var jane = new Adult("Jane Smith", "Orange");
-jane.greet();
-jane.payTaxes();
+var first  = new MobileMenu();
